05b62edf0f5b84a3ba47dfe72c55ef795472977c
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-10-08 14:36:21 -0500

    Se finalizo la pagina de muestra del producto

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -82,7 +82,24 @@ class _ProductDetailsState extends State<ProductDetails> {
             children: <Widget>[
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text("Size"),
+                            content: new Text("Chooze the size"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text("Close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
                   color: Colors.white,
                   textColor: Colors.grey,
                   elevation: 2.0,
@@ -100,7 +117,24 @@ class _ProductDetailsState extends State<ProductDetails> {
               ), //Expanded
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text("Color"),
+                            content: new Text("Chooze the color"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text("Close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
                   color: Colors.white,
                   textColor: Colors.grey,
                   elevation: 2.0,
@@ -118,7 +152,24 @@ class _ProductDetailsState extends State<ProductDetails> {
               ), //Expanded
               Expanded(
                 child: MaterialButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: new Text("Quantity"),
+                            content: new Text("Chooze the Quantity"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: new Text("Close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
                   color: Colors.white,
                   textColor: Colors.grey,
                   elevation: 2.0,
@@ -155,8 +206,68 @@ class _ProductDetailsState extends State<ProductDetails> {
                   onPressed: () {})
             ],
           ), //Row
-        ],
-      ), //ListView
-    ); //Scaffold,);
+          Divider(),
+          new ListTile(
+            title: new Text("Product Details"),
+            subtitle: new Text(
+              "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+              textAlign: TextAlign.justify,
+            ), //Text
+          ), //Listtile
+          Divider(),
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "Product Name: ",
+                  style: TextStyle(color: Colors.grey),
+                ), //Text
+              ), //Padding
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text(widget.product_detail_name),
+              )
+            ], //Widget
+          ), //Row
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "Product brand:",
+                  style: TextStyle(color: Colors.grey),
+                ), //Text
+              ), //Padding
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "GUCCI",
+                  style: TextStyle(color: Colors.grey),
+                ), //Text
+              ), //Padding
+            ],
+          ), //Row
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "Product condition:",
+                  style: TextStyle(color: Colors.grey),
+                ), //Text
+              ), //Padding
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+                child: new Text(
+                  "NEW",
+                  style: TextStyle(color: Colors.grey),
+                ), //Text
+              ), //Padding
+            ],
+          ), //Row
+        ], //Widget
+      ), //Listview,
+    );
   }
 }
