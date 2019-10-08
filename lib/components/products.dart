e46ac2cae939010f53802ed4a93e30c828bfc691
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-10-07 23:12:56 -0500

    Se implemento pasar la imagen del producto seleccionado a un nuevo View

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -15,7 +15,7 @@ class _ProductsState extends State<Products> {
       "price": 85,
     },
     {
-      "name": "Blazer",
+      "name": "Blazer2",
       "picture": "images/products/blazer2.jpeg",
       "old_price": 120,
       "price": 85,
@@ -65,7 +65,12 @@ class Single_prod extends StatelessWidget {
       child: Material(
         child: InkWell(
           onTap: () => Navigator.of(context).push(new MaterialPageRoute(
-              builder: (context) => new ProductDetails())),
+              builder: (context) => new ProductDetails(
+                    product_detail_name: product_name,
+                    product_detail_new_price: prod_price,
+                    product_detail_old_price: prod_old_price,
+                    product_detail_picture: prod_picture,
+                  ))),
           child: GridTile(
             footer: Container(
               color: Colors.white70,
--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
+  final product_detail_name;
+  final product_detail_new_price;
+  final product_detail_old_price;
+  final product_detail_picture;
+  ProductDetails(
+      {this.product_detail_name,
+      this.product_detail_new_price,
+      this.product_detail_old_price,
+      this.product_detail_picture});
+
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
 }
@@ -8,6 +18,39 @@ class ProductDetails extends StatefulWidget {
 class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.black,
+        title: Text('Shopping App'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ],
+      ), //Appbar
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+            height: 300.0,
+            child: GridTile(
+              child: Container(
+                color: Colors.white,
+                child: Image.asset(widget.product_detail_picture),
+              ),
+            ),
+          )
+        ],
+      ),
+    ); //Scaffold,);
   }
 }
