fae8bf3a3064b15185aa5d5ef58f59bc9c88d7ba
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-10-16 08:01:31 -0500

    Primera parte del Login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecom/pages/home.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,6 +14,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final GoogleSignIn googleSignIn = new GoogleSignIn();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   SharedPreferences preferences;
   bool loading = false;
   bool isLogedin = false;
@@ -42,5 +45,53 @@ class _LoginState extends State<Login> {
     setState(() {
       loading = false;
     });
+  }
+
+  Future handleSignIn() async {
+    preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      loading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleUser.authentication;
+    AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    FirebaseUser firebaseUser =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+
+    if (firebaseUser != null) {
+      final QuerySnapshot result = await Firestore.instance
+          .collection("users")
+          .where("id", isEqualTo: firebaseUser.uid)
+          .getDocuments();
+      final List<DocumentSnapshot> documents = result.documents;
+
+      if (documents.length == 0) {
+        Firestore.instance
+            .collection("users")
+            .document(firebaseUser.uid)
+            .setData({
+          "id": firebaseUser.uid,
+          "username": firebaseUser.displayName,
+          "profilePicture": firebaseUser.photoUrl
+        });
+        await preferences.setString("id", firebaseUser.uid);
+        await preferences.setString("username", firebaseUser.displayName);
+        await preferences.setString("photoUrl", firebaseUser.displayName);
+      } else {
+        await preferences.setString("id", documents[0]['id']);
+        await preferences.setString("username", documents[0]['username']);
+        await preferences.setString("photoUrl", documents[0]['photoUrl']);
+      }
+
+      Fluttertoast.showToast(msg: "Login was successful");
+      setState(() {
+        loading = false;
+      });
+    } else {}
   }
 }
