7df0acff2c30a543bfb4ad6b5b373c3746efb53d
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-10-16 00:20:17 -0500

    Se integro Firebase a la App
    Se configuro todo el Firebase para iniciar la implementación del Login y de la Real Time Database

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <firebase_core/FirebaseCorePlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
 }
 
 @end
